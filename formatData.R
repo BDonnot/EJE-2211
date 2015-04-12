7633168a5e8bb4ba19eca10673096befc9d5eeeb
Benjamin DONNOT <benjamin.donnot@gmail.com>
2015-04-12 20:42:47 +0200

    add comments

--- formatData.R
+++ formatData.R
@@ -32,87 +32,115 @@ table = fread("data/Costes_compte_x1x2_20150311.csv", #le nom du document
 ##entiers par exemple)
 ##et ca permet aussi de se rappeler de quel type sont les colonnes creees dans la suite du code :-)
 
+#########################
+##Formatage des donnees##
+#########################
+#les donnees sont parfois mal formatees
+#les quelques fonctions ci-dessous vont enlever les 'NULL' ou les '{' par exemple
+#afin de faciliter les futurs traitements
+
+#le vecteur ci-dessous contient toutes les colonnes sur lesquelles nous allons travailler :
 colConcerned = c("mises_max_heure","mises_max_jour","mises_max_semaine","mises_max_mois",
                  "depots_max_heure","depots_max_jour","depots_max_semaine","depots_max_mois",
                  "limites_retraits")
 
 removeNull = function(charVect)
 {
-  #cette fonction va retirer la chaine de caractere "NULL" quand elle va la voir
+  #cette fonction va retirer la chaine de caractere "NULL" 
+  #quand elle va la voir dans chaque element de charVect
   return(gsub("NULL","",charVect))
 }
 removeComa = function(charVect)
 {
-  #cette fonction va retirer toutes les sequences de plusieurs virgules a la suite
+  #cette fonction va retirer toutes les sequences de plusieurs virgules a la suite 
+  #quand elle va la voir dans chaque element de charVect
   return(gsub(",+",",",charVect))  
 }
 removeBraceComa = function(charVect)
 {
-  #cette fonction va retirer toutes les sequences de la forme '{,' ou ',}'
+  #cette fonction va retirer toutes les sequences de la forme '{,' ou ',}' 
+  #quand elle va la voir dans chaque element de charVect
   return(gsub(",*\\}","",gsub("\\{,*","",charVect)))  
 }
 removeDoubleBrace = function(charVect)
 {
   #cette fonction va retirer toutes les sequences de la forme '{' ou '}'
+  #quand elle va la voir dans chaque element de charVect
   return(gsub("\\{","",gsub("\\}","",charVect)))
 }
 removeQuote = function(charVect)
 {
   #cette fonction va retirer tous les guillemets
+  #quand elle va les voir dans chaque element de charVect
   return(gsub("\"","",charVect))
 }
-#on applique toutes les fonctions precedentes dans l'ordre :
+
+#on applique toutes les fonctions precedentes dans l'ordre au colonnes selectionnees
 table[,
       (colConcerned) := lapply(.SD,function(x) removeQuote(removeDoubleBrace(removeBraceComa(removeComa(removeNull(x)))))  ),
       .SDcols = colConcerned]
+#il y a plusieurs 'astuces' ici.
+#La permier est l'emploi de .SDcols, qui permet de specifier les colonnes concernees
+#La seconde est l'emploi de lapply(.SD,...) qui va permettre d'appliquer à chaque colonne la fonction '...'
+##et de renvoyer la colonne ainsi modifiee
+#La troisieme est l'emploi de '(colConcerned) :=' qui est la facon de dire a R de ne pas creer de
+##nouvelles colonnes, mais de modifier les anciennces
+#Pour plus d'informations, on pourra taper la commande ?data.table
+#########################
+##nombre de changements##
+#########################
 
-
-#on enleve les changements qui n'en sont pas vraiment : par exemple :
-# "2012-10-13 02:33:04#100#100" : le chamgement etait de 100 à 100, donc pas de changement
-# dealOneDate = function(oneDate)
-# {
-#   temp = strsplit(oneDate,"#")
-#   res = sapply(temp,function(x) ifelse( length(x)==0 | x[2] == x[3] ,"",paste0(x,collapse = '#') ) )
-#   return(res)
-# }
-# agregateDates = function(x)
-# {
-#   temp = sapply(x,dealOneDate)
-#   paste0(temp[nchar(temp) > 0],collapse = ',' )
-# }
-# keepOnlyTrueChanges = function(x)
-# {
-#   temp = strsplit(x,",")
-#   sapply(temp,function(vectPossibleChange) agregateDates(vectPossibleChange) ) 
-# }
-# table[,
-#       (colConcerned) := lapply(.SD,keepOnlyTrueChanges),
-#       .SDcols = colConcerned]
-
-#nombre de changements
+#le vecteur newcol va representer dans la suite les nouveaux noms des variables.
+#Les nouveaux noms sont composes des anciens noms auxquels on a rajoute
+#la chaine de caracteres "_Chgt". Par exemple on a transforme
+#depots_max_semaine -> depots_max_semaine_Hausse_Chgt
 newCol = paste(colConcerned,"Chgt",sep = "_")
+
 countNb = function(x)
 {
+  #cette fonction compte le nombre de changements (a la hausse ou a la baisse)
+  
+  #sapply(vect/list,fun) va appliquer la fonction "fun" a chaque element
+  #du vecteur 'vect' (ou de la liste 'list') et mettre tous les resultats
+  #dans un unique vecteur
+  #on pourra regarder l'aide via ?sapply
+  
+  #strsplit(charVect,char) va 'splitter' (ie decouper) chaque element du vecteur
+  #de chaine de character 'charVect' en fonction de la chaine de character
+  #'char' et les mettre tous dans un vecteur. Ces vecteurs seront ensuite
+  #mis les uns au bout des autres dans une list.
+  #on pourra consulter l'aider via ?strsplit
   sapply(strsplit(x,","),length)
 }
 
+#on applique la fonction precedentes aux variables selectionnees :
 table[,
       (newCol) := lapply(.SD,countNb),
       .SDcols = colConcerned]
-#nbs de changements a la hausse
-newCol = paste(colConcerned,"Baisse",sep = "_")
-countNb = function(x)
-{
-  sapply(strsplit(x,","),function(y) length(diff(y)) )
-}
-table[,
-      (newCol) := lapply(.SD,countNb),
-      .SDcols = colConcerned]
+#On peut remarquer l'emploi de la meme syntaxe que precemment.
+#.SDcols = colConcerned : les modifications concernent les memes colonnes
+
+# lapply(.SD,countNb) va toujours appliquer la fonction countNb a chaque colonnes
+# appelees par .SDcols (appelees via .SD) et renvoyer unes liste de colonnes
+
+#on va ensuite donner a ces colonnes les noms voulus via la commande  '(newCol) :='
+#il ne faut pas oublier les parentheses, sinon R va croire qu'il s'agit de la creation d'une seule
+#colonne nomme 'newCol'
+
 
 #nbs de changements a la baisse puis a la hausse
 extractValue = function(x)
 {
-  sapply(x,function(y) ifelse(grepl("#",y),as.numeric(strsplit(y,'#')[[1]][2]),0))
+  #as.numeric va convertir la chaine de character en un nombre
+  
+  #grepl(char,stringVect) va renvoyer un vecteur res contenant autant d'elements
+  #que stringVect. res contiendra 'True' ou 'False' selon que le character 'char'
+  #est contenu dans la chaine testee.
+  
+  #ifelse(vectBool,valeurSiVrai,valeurSiFaux) va renvoyer un vecteur 'res' de meme longeur
+  #que vectBool. Chaque element de 'res' correspondera a 'valeurSiVrai' ou 'valeurSiFaux'
+  #selon que l'element vectBool est vrai ou faux
+  sapply(x,function(y) ifelse(grepl("#",y),as.numeric(strsplit(y,'#')[[1]][2]),0) )
 }
 countChanges = function(x,fun = countBaisse)
 {
@@ -122,14 +150,26 @@ countChanges = function(x,fun = countBaisse)
 
 countBaisse = function(x)
 {
+  #diff(numericVect) va calculer la difference (au sens des series temporelles) du vecteur
+  #de nombres 'numericVect'
+  #par exemple
+  #numericVect = c(1,2,4,7), 
+  #diff(numericVect)
+  #sera le vecteur c(1,2,3)
   temp = diff(x)
   length(temp[temp<0])
 }
+
+#on cree de nouveaux noms de colonnes en rajoutant "_Baisse"
+#exactement de la meme facon qu'on l'avait fait precedemment
+#avec "_Chgt"
 newCol = paste(colConcerned,"Baisse",sep = "_")
+#et creer les variables dont on a specifie le nom precedemment
 table[,
       (newCol) := lapply(.SD,countChanges,fun = countBaisse),
       .SDcols = colConcerned]
-unique(table[,depots_max_mois_Baisse])
+
+#on fait la meme chose pour compter les 'hausses'
 countHauuse= function(x)
 {
   temp = diff(x)
@@ -139,11 +179,23 @@ newCol = paste(colConcerned,"Hausse",sep = "_")
 table[,
       (newCol) := lapply(.SD,countChanges,fun = countHauuse),
       .SDcols = colConcerned]
-unique(table[,depots_max_mois_Hausse])
+
 
 ##On ecrit la table :
-write.table(table,
-            "data/Costes_compte_x1x2_20150311Clean.csv",
-            sep = ";",
-            row.names = F,
-            col.names = T)
+write.table(table, #on veut ecrire la table nommee 'table'
+            "data/Costes_compte_x1x2_20150311Clean.csv", #dans le fichier 'Costes_compte_x1x2_20150311Clean.csv', situe dans le dossier "data/"
+            sep = ";", #specifie le separateur des colonnes
+            row.names = F, #les lignes n'ont pas noms particulier, on ne rajoutera donc pas une colonne du type 'nom des lignes'
+            col.names = T) #on specifie par contre qu'il faut ecrire le nom des colonnes !
+
+#il y a pas mal de fonctions dans ce petit script
+#meme si j'ai essaye de les commenter, je pense que ce n'est pas toujours facile a comprendre
+#les fonctions sur lesquelles on pourra s'attarder sont :
+#apply,sapply,lapply
+#fread : pour lire une table depuis le disque dur
+#write.table : pour ecrire une table sur le disque dur
+
+#les autres sont soit tres specifiques, soit un peu trop compliquees pour bien comprendre
+#le code ici est assez optimise niveau temps de calcul et memoire utilisee.
+#on pourrait faire des choses un peu plus facile a comprendre en faisant des boucles 'for' au lieu
+#des choses de type "apply" "sapply" ou "lapply" par exemple.
